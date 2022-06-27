7a896198e41e8de6fc683f4308d19ca9dbaa990d
Azis Naufal <azis.naufal@gmail.com>
2022-06-27 22:27:26 +0700

    setup viewmodel

---
+++ app/src/main/java/ac/id/unikomc/trymvvm/MainViewModel.java
@@ -0,0 +1,30 @@
+package ac.id.unikomc.trymvvm;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    private final MutableLiveData<Celsius> celsius;
+
+    public MainViewModel() {
+        celsius = new MutableLiveData<>();
+    }
+
+    public LiveData<Celsius> getCelsius() {
+        return celsius;
+    }
+
+    public void setCelsius(String stringCelsius) {
+        if (stringCelsius.isEmpty())
+            stringCelsius = "0";
+
+        float parsedCelsius = Float.parseFloat(stringCelsius);
+
+        Celsius celsius = new Celsius();
+        celsius.setCelsius(parsedCelsius);
+
+        this.celsius.postValue(celsius);
+    }
+}

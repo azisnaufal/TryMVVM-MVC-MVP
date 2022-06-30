29a019cb8fe38d02f28ca70402616a35284dc074
Azis Naufal <azis.naufal@gmail.com>
2022-06-30 22:28:32 +0700

    rename

--- app/src/androidTest/java/ac/id/unikom/tryccdp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ac/id/unikom/tryccdp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ac.id.unikom.trymvvm;
+package ac.id.unikom.tryccdp;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ac.id.unikom.trymvvm", appContext.getPackageName());
+        assertEquals("ac.id.unikom.tryccdp", appContext.getPackageName());
     }
 }

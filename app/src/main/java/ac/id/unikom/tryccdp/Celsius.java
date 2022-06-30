daf4143fd21da0cb4c29a4c39e8636b68474b589
Azis Naufal <azis.naufal@gmail.com>
2022-07-01 00:08:41 +0700

    setup model to be observable

--- app/src/main/java/ac/id/unikom/tryccdp/Celsius.java
+++ app/src/main/java/ac/id/unikom/tryccdp/Celsius.java
@@ -1,17 +1,39 @@
 package ac.id.unikom.tryccdp;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 public class Celsius {
     private double celsius;
+
+    private MutableLiveData<Double> reamur;
+    private MutableLiveData<Double> fahrenheit;
+
+    public Celsius() {
+        this.celsius = 0;
+        this.reamur = new MutableLiveData<>();
+        this.fahrenheit = new MutableLiveData<>();
+    }
+
+    public LiveData<Double> getReamur() {
+        return reamur;
+    }
+
+    public LiveData<Double> getFahrenheit() {
+        return fahrenheit;
+    }
 
     public void setCelsius(double celsius) {
         this.celsius = celsius;
     }
 
-    public double toReamur() {
-        return 0.8 * celsius;
+    public void toReamur() {
+        double reamur = 0.8 * celsius;
+        this.reamur.setValue(reamur);
     }
 
-    public double toFahrenheit() {
-        return (1.8 * celsius) + 32;
+    public void toFahrenheit() {
+        double fahrenheit = (1.8 * celsius) + 32;
+        this.fahrenheit.setValue(fahrenheit);
     }
 }

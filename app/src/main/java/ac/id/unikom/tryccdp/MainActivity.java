884c913f415f5d332458d61e5946e495e9580814
Azis Naufal <azis.naufal@gmail.com>
2022-07-01 00:22:53 +0700

    integrate view with controller and model

--- app/src/main/java/ac/id/unikom/tryccdp/MainActivity.java
+++ app/src/main/java/ac/id/unikom/tryccdp/MainActivity.java
@@ -1,24 +1,40 @@
 package ac.id.unikom.tryccdp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainView {
 
     private EditText celsius;
     private EditText fahrenheit;
     private EditText reamur;
+    private MainController controller;
+    private Celsius model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controller = new MainController(this);
+        model = Celsius.getInstance();
 
         initView();
+        observeModel();
+    }
+
+    private void observeModel() {
+        model.getFahrenheit().observe(this, fahrenheit -> {
+            this.fahrenheit.setText(fahrenheit);
+        });
+
+        model.getReamur().observe(this, reamur -> {
+            this.reamur.setText(reamur);
+        });
     }
 
     private void initView() {
@@ -39,8 +55,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
-
+                controller.calculateTemperature();
             }
         });
+    }
+
+    @Override
+    public String getCelsius() {
+        return celsius.getText().toString();
     }
 }
